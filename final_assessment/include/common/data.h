fb3b815d419de27a6851378d03ba228a03a00017
Lucas Cunha <lucasgb7_ks@hotmail.com>
2023-07-11 14:56:39 -0300

    :bricks: Setting template for the assessment

---
+++ final_assessment/include/common/data.h
@@ -0,0 +1,24 @@
+/******************************************************************************
+ * Copyright (C) 2017 by Alex Fosdick - University of Colorado
+ *
+ * Redistribution, modification or use of this software in source or binary
+ * forms is permitted as long as the files maintain this copyright. Users are 
+ * permitted to modify this and use it to learn about the field of embedded
+ * software. Alex Fosdick and the University of Colorado are not liable for any
+ * misuse of this material. 
+ *
+ *****************************************************************************/
+/**
+ * @file data.h
+ * @brief Data library
+ *
+ * Data library file
+ *
+ * @author Lucas Cunha
+ * @date 11-07-2023
+ *
+ */
+#ifndef __DATA_H__
+#define __DATA_H__
+
+#endif /* __DATA_H__ */
