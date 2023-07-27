1d8dcaadc86456dbb409521ab8b8d86bfcddc513
Lucas Cunha <lucasgb7_ks@hotmail.com>
2023-07-27 11:04:03 -0300

    :sparkles: feat: Adding data manipulation functions

--- final_assessment/include/common/data.h
+++ final_assessment/include/common/data.h
@@ -15,10 +15,37 @@
  * Data library file
  *
  * @author Lucas Cunha
- * @date 11-07-2023
+ * @date July 26 2023
  *
  */
 #ifndef __DATA_H__
 #define __DATA_H__
+#include <stdint.h>
+
+/**
+ * @brief Integer-to-ASCII converts data from a standard integer type into an ASCII string.
+ * 
+ * Converts a passed data in as a signed 32-bit integer given a base (2 or 16).
+ * 
+ * @param data the number you wish to convert passed as a signed 32-bit integer.
+ * @param ptr converted character string to the uint8_t* pointer.
+ * @param base support bases 2 to 16 (decimal/hexadecimal).
+ *
+ * @return the length of the converted data.
+ */
+uint8_t my_itoa(int32_t data, uint8_t *ptr, uint32_t base); 
+
+/**
+ * @brief ASCII-to-Integer converts data back from an ASCII represented string into an integer type.
+ * 
+ * Converts a passed data in as a signed 32-bit string given a base (2 or 16).
+ * 
+ * @param ptr converted character string to the uint8_t* pointer. 
+ * @param digits the number you wish to convert passed as a signed 8-bit integer.
+ * @param base support bases 2 to 16 (decimal/hexadecimal).
+ *
+ * @return the length of the converted data.
+ */
+int32_t my_atoi(uint8_t *ptr, uint8_t digits, uint32_t base);
 
 #endif /* __DATA_H__ */
